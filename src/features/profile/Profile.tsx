f724f61e5bea461372ccf92658d558ec2763e47b
andreypats <andrey.it.work@gmail.com>
2022-12-14 00:42:41 +0300

    Add base personal information to Profile.tsx

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export const Profile = () => {
-    return (
-        <div>
-            Profile
-        </div>
-    );
+  return (
+    <div>
+      <h3>Personal information</h3>
+      <div>
+        <img
+          style={{ height: "100px" }}
+          src="https://st4.depositphotos.com/1012074/25277/v/600/depositphotos_252773324-stock-illustration-young-avatar-face-with-sunglasses.jpg"
+          alt="profile photo"
+        />
+      </div>
+      <div>Name</div>
+      <div>Email</div>
+      <button>Logout</button>
+    </div>
+  );
 };
