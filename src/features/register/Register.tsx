347fefc8dd164ad0972873bcbc0d174d3b16de84
nikita <nikskozlov@gmail.com>
2022-12-14 19:20:18 +0300

    fix: fix esLint errors

--- src/features/register/Register.tsx
+++ src/features/register/Register.tsx
@@ -1,32 +1,22 @@
-import React from "react";
-import {FormGroup, TextField} from "@mui/material";
-import {useFormik} from "formik";
+import React from 'react'
+
+import { FormGroup, TextField } from '@mui/material'
+import { useFormik } from 'formik'
 
 export const Register = () => {
+  const formik = useFormik({
+    validate: values => {},
+    initialValues: {},
+    onSubmit: values => {},
+  })
 
-    const formik = useFormik({
-        validate: (values) => {
-
-        },
-        initialValues: {},
-        onSubmit: values => {
-
-        }
-    })
-
-    return (
-        <form onSubmit={formik.handleSubmit}>
-            <FormGroup>
-                <TextField
-                    label="Email"
-                />
-                <TextField
-                    label="Password"
-                />
-                <TextField
-                    label="Confirm password"
-                />
-            </FormGroup>
-        </form>
-    )
-};
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <FormGroup>
+        <TextField label="Email" />
+        <TextField label="Password" />
+        <TextField label="Confirm password" />
+      </FormGroup>
+    </form>
+  )
+}
