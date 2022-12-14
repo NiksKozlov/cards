567bfb39f4af48af9a1ce570755a7ef78dd64d7d
nikita <nikskozlov@gmail.com>
2022-12-14 18:44:33 +0300

    fix: esLint errors

--- src/features/register/Register.tsx
+++ src/features/register/Register.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from 'react'
 
 export const Register = () => {
-    return (
-        <div>
-            Registration page
-        </div>
-    );
-};
+  return <div>Registration page</div>
+}
