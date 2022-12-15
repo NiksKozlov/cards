94bd75de18c41bb54401994d4dfd1127cb5a9dd8
nikita <nikskozlov@gmail.com>
2022-12-15 17:11:00 +0300

    fix: esLint errors fixed

--- src/features/forgot-password/ForgotPassword.tsx
+++ src/features/forgot-password/ForgotPassword.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from 'react'
 
 export const ForgotPassword = () => {
-    return (
-        <div>
-            Forgot password page
-        </div>
-    );
-};
+  return <div>Forgot password page</div>
+}
